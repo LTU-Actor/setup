81270c1ab4f8ec78a7e12f280a5f1cb7d09b9ac6
nick-paul <npaul@ltu.edu>
2021-09-14 20:05:13 -0400

    update for noetic

---
+++ setup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+if [ "$(id -u)" == "0" ] && [ -z "$SUDO_USER" ]; then
+  echo "Don't run as root."
+  exit -1
+fi
+
+# ensure root permissions
+if [ "$(id -u)" != "0" ]; then
+  exec sudo "$0" "$@"
+fi
+
+apt update
+apt upgrade -y
+
+apt install -y \
+  curl \
+  fish \
+  vim \
+  nvim \
+  git \
+  nodejs \
+  build-essential \
+  cmake \
+  htop \
+  openssh-server \
+  clang \
+  python \
+  python3 \
+  python-is-python3
+
+if [ ! -e /opt/ros/noetic/ ]; then
+    sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+    apt update
+
+    apt install ros-noetic-desktop-full
+
+    source /opt/ros/noetic/setup.bash
+
+    sudo apt install python3-rosdep
+
+    rosdep init
+
+    rosdep update
+fi
+
+
+
+sudo -u $SUDO_USER bash << EOF
+
+  if ! grep -Fq "/opt/ros" "${HOME}/.bashrc"; then
+    echo 'source /opt/ros/noetic/setup.bash' >> "${HOME}/.bashrc"
+  fi
+
+EOF
