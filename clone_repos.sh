cac3d2ceaef232548db556704b137ae9efda917e
nick-paul <npaul@ltu.edu>
2021-09-21 19:28:56 -0400

    Add script for cloning core actor repos

---
+++ clone_repos.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/actor_ws/src
+cd ~/actor_ws/src
+
+git clone https://github.com/LTU-Actor/setup.git
+git clone https://github.com/LTU-Actor/Route-Blob.git
+git clone https://github.com/LTU-Actor/Route-Spline.git
+git clone https://github.com/LTU-Actor/RPi-eStop-Loop.git
+git clone https://github.com/LTU-Actor/Route-Obstacle.git
+git clone https://github.com/LTU-Actor/Route-StopSign.git
+git clone https://github.com/LTU-Actor/RPi-GPIO.git
+git clone https://github.com/LTU-Actor/Core.git
+git clone https://github.com/LTU-Actor/eStop.git
+git clone https://github.com/LTU-Actor/InputProcess-CamAdjust.git
+git clone https://github.com/LTU-Actor/Route-Pothole.git
+git clone https://github.com/LTU-Actor/Route-Waypoint.git
+git clone https://github.com/LTU-Actor/Vehicle-GEM.git
