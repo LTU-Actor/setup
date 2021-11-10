1f7370068cb3fe223caca4676f2ac7c72cefc466
Mitchell Pleune <mitchpleune@gmail.com>
2021-11-09 20:26:10 -0500

    Fix crash with vimba camera on 20.04 & wifi issues

--- install_vimba.sh
+++ install_vimba.sh
@@ -3,18 +3,31 @@
 set -e
 
 if compgen -G "/opt/Vimba*" > /dev/null; then
-	echo "Vimba already exists in /opt/! Exiting..."
-	exit 0
+	echo "Vimba already exists in /opt/! Skipping..."
+else
+	wget "https://downloads.alliedvision.com/Vimba_v5.0_Linux.tgz" -O "/tmp/Vimba_v5.0_Linux.tgz"
+
+	sudo mkdir -p /opt/
+	sudo tar xf "/tmp/Vimba_v5.0_Linux.tgz" -C /opt/
+	rm /tmp/Vimba*
+
+	sudo /opt/Vimba*/VimbaUSBTL/Install.sh
+	sudo /opt/Vimba*/VimbaGigETL/Install.sh
+
+	echo "Installation scripts have been executed..."
+	echo "Please log out/in, or reboot before they take effect."
 fi
 
-wget "https://downloads.alliedvision.com/Vimba_v5.0_Linux.tgz" -O "/tmp/Vimba_v5.0_Linux.tgz"
+if [[ -e ~/actor_ws/src/avt_vimba_camera ]]; then
+	echo "ROS node source already cloned, skipping!"
+	echo "remove ~/actor_ws/src/avt_vimba_camera to reinstall"
+else	
+	mkdir -p ~/actor_ws/src
+	cd ~/actor_ws/src
 
-sudo mkdir -p /opt/
-sudo tar xf "/tmp/Vimba_v5.0_Linux.tgz" -C /opt/
-rm /tmp/Vimba*
+	git clone https://github.com/astuff/avt_vimba_camera.git
 
-sudo /opt/Vimba*/VimbaUSBTL/Install.sh
-sudo /opt/Vimba*/VimbaGigETL/Install.sh
-
-echo "Installation scripts have been executed..."
-echo "Please log out/in, or reboot before they take effect."
+	# must replace the library files with the recent versions to prevent segfaults on 20.04
+	cp -f /opt/Vimba_5_0/VimbaCPP/DynamicLib/x86_64bit/libVimbaC.so avt_vimba_camera/lib/64bit/libVimbaC.so
+	cp -f /opt/Vimba_5_0/VimbaCPP/DynamicLib/x86_64bit/libVimbaCPP.so avt_vimba_camera/lib/64bit/libVimbaCPP.so
+fi
